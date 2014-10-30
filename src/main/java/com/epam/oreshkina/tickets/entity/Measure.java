39d7d65de15f6896ec5820d1f07fe76298c3733b
viktoriyaoreshkina <vikus-jesus@mail.ru>
2014-10-30 17:26:44 +0600

    initial commit

---
+++ src/main/java/com/epam/oreshkina/tickets/entity/Measure.java
@@ -0,0 +1,7 @@
+package com.epam.oreshkina.tickets.entity;
+
+/**
+ * Created by Viktoriya_Oreshkina on 28-Oct-14.
+ */
+public class Measure {
+}
---
+++ src/main/java/com/epam/oreshkina/tickets/entity/Role.java
@@ -0,0 +1,9 @@
+package com.epam.oreshkina.tickets.entity;
+
+/**
+ * Created by Viktoriya_Oreshkina on 28-Oct-14.
+ */
+//The role may be client, seller or admin of Database.
+
+public class Role {
+}
