d634fc00f95b407e07168229a7f69996b04f713c
viktoriyaoreshkina <vikus-jesus@mail.ru>
2014-11-04 14:29:33 +0600

    fields, Ures and Classes added

--- src/main/java/com/epam/oreshkina/tickets/entity/Order.java
+++ src/main/java/com/epam/oreshkina/tickets/entity/Order.java
@@ -1,7 +1,12 @@
-package com.epam.oreshkina.tickets.entity;
+package epam.tickets;
 
 /**
  * Created by Viktoriya_Oreshkina on 28-Oct-14.
  */
 public class Order {
+    //field
+    private Integer id;
+    //как сделать несколько заказов - List
+
+
 }
--- src/main/java/com/epam/oreshkina/tickets/entity/Role.java
+++ src/main/java/com/epam/oreshkina/tickets/entity/Role.java
@@ -1,9 +1,15 @@
-package com.epam.oreshkina.tickets.entity;
+package epam.tickets;
+
+import static java.lang.Math.pow;
 
 /**
  * Created by Viktoriya_Oreshkina on 28-Oct-14.
  */
 //The role may be client, seller or admin of Database.
 
+
 public class Role {
+    public static void main(String[] args) {
+        int a = pow(2,4);
+    }
 }
